44d13e8e192cf4c52dcceaa838a9f3fe44887b4c
omj4085 <151400868+omj4085@users.noreply.github.com>
2025-02-08 14:18:51 -0500

    players.c update

--- jeopardy_source/jeopardy_source/players.c
+++ jeopardy_source/jeopardy_source/players.c
@@ -13,6 +13,15 @@
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
 {
+//this for loop will go through the list of the 4 players
+    for(int i = 0; 1<num_players; i++){
+//strcmp is to compare the players name with a given name and return an int for which string is bigger
+        if (strcmp(players [i].name, name) ==0){
+//if the players name matches with an existing one then it returns true
+            return true;
+        }
+    }
+//returns false if the player doesnt exist
     return false;
 }
 
@@ -20,5 +29,14 @@ bool player_exists(player *players, int num_players, char *name)
 // player given their name
 void update_score(player *players, int num_players, char *name, int score)
 {
-    
+//loops through the list of players
+    for(int i=0; i<num_players; i++){
+//cimpares player name with given anem
+        if(strcmp(players[i].name, name)==o){
+//updates the players score
+            players[i].score += score;
+//leaves loop once the player is found
+            break;
+        }
+    }
 }
