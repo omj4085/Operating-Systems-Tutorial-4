49601cc2653141bd9d8707f1f34e242784152347
riya-rajesh <166192117+riya-rajesh@users.noreply.github.com>
2025-02-09 15:49:57 -0500

    Update jeopardy.c

--- jeopardy_source/jeopardy_source/jeopardy.c
+++ jeopardy_source/jeopardy_source/jeopardy.c
@@ -24,16 +24,47 @@
 // Put global environment variables here
 
 // Processes the answer from the user containing what is or who is and tokenizes it to retrieve the answer.
-void tokenize(char *input, char **tokens);
+void tokenize(char *input, char **tokens){
+  
+  //tokenizes using spaces
+  char *token = strtok(input, " ");
+  
+  token = strtok(NULL, " "); //gets rid of first word: what or who
+  token = strtok(NULL, " "); //gets rid of second word: is
+  
+  //third word should now be the answer
+  *tokens = token;
+  
+}
 
 // Displays the game results for each player, their name and final score, ranked from first to last place
-void show_results(player *players, int num_players);
+void show_results(player *players, int num_players){
+  
+  //first to last place means descending score order
+  //compare two players i and j
+  for (int i=0; i<num_players-1;i++){ 
+    for (int j=i+1; j<num_players; j++){
+      if (players[i].score < players[j].score){ //swap players[i] and players[j]
+        player temp = players[i];
+        players[i] = players[j];
+        players[j] = temp;
+      }
+    }
+  } 
+  
+  //print ranks
+  printf("Game Result: ");
+  for (inti=0; i<num_players; i++){
+    printf("Name: %s  Final Score: %d \n", players[i].name, players[i].score);
+  }
+      
+}
 
 
 int main(int argc, char *argv[])
 {
     // An array of 4 players, may need to be a pointer if you want it set dynamically
-    struct player players[NUM_PLAYERS]; //supposed to be a struct?
+    struct player players[NUM_PLAYERS]; 
     
     // Input buffer and and commands
     char buffer[BUFFER_LEN] = { 0 };
