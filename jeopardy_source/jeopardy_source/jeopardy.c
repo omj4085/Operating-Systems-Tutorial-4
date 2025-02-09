323dfa1fd6d9f0be60a3f336dcb5646a4eed677b
riya-rajesh <166192117+riya-rajesh@users.noreply.github.com>
2025-02-09 18:22:15 -0500

    Update jeopardy.c

--- jeopardy_source/jeopardy_source/jeopardy.c
+++ jeopardy_source/jeopardy_source/jeopardy.c
@@ -145,7 +145,7 @@ int main(int argc, char *argv[]) {
         }
 
         //check for game's end
-        if (total_answered >= TOTAL_QUESTIONS) {
+        if (total_answered >= 12) {
             game_over = true;
         }
 
