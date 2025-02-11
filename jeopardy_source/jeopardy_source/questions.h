b76f1716d11770879cadd5eb9693eb686df340ab
Paris Scasserra <166191144+pariiss@users.noreply.github.com>
2025-02-10 23:43:09 -0500

    Update questions.h

--- jeopardy_source/jeopardy_source/questions.h
+++ jeopardy_source/jeopardy_source/questions.h
@@ -17,11 +17,7 @@
 #define NUM_QUESTIONS 12
 
 // List of 3 categories as array of strings
-static char categories[NUM_CATEGORIES][MAX_LEN] = {
-    "Computers", 
-    "Animals", 
-    "Food"
-};
+extern char categories[NUM_CATEGORIES][MAX_LEN];
 
 // Questions struct for each question
 typedef struct {
@@ -32,23 +28,23 @@ typedef struct {
     bool answered;
 } question;
 
-// An array of 12 questions (4 for each category), initialized in initialize_game
-// this may need to be a pointer if you want it set dynamically
-question questions[NUM_QUESTIONS];
+//array of 12 questions (4 for each category), initialized in initialize_game
+//this may need to be a pointer if you want it set dynamically
+extern question questions[NUM_QUESTIONS];
 
-// Initializes the array of questions for the game
+//Initializes the array of questions for the game
 extern void initialize_game(void);
 
-// Displays each of the remaining categories and question dollar values that have not been answered
+//displays each of the remaining categories and question dollar values that have not been answered.
 extern void display_categories(void);
 
-// Displays the question for the category and dollar value
+//Displays the question for the category and dollar value
 extern void display_question(char *category, int value);
 
-// Returns true if the answer is correct for the question for that category and dollar value
+//returns true if the answer is correct for the question for that category and dollar value
 extern bool valid_answer(char *category, int value, char *answer);
 
-// Returns true if the question has already been answered
+//returns true if the question has already been answered
 extern bool already_answered(char *category, int value);
 
 #endif /* QUESTIONS_H_ */
